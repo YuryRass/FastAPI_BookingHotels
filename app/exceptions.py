273592c6abf5277f711915cdd48246b8732aeed4
Yurii <yurchik1997yurchik@gmail.com>
2023-12-11 17:27:41 +0300

    add exceptions for hotels

--- app/exceptions.py
+++ app/exceptions.py
@@ -2,57 +2,54 @@
 from fastapi import HTTPException, status
 
 
-class UserUnauthorizedException(HTTPException):
-    status_code = status.HTTP_409_CONFLICT
-    detail = "User is unauthorized"
-
-    def __init__(self):
-        super().__init__(status_code=self.status_code, detail=self.detail)
-
-
-class IncorrectJWTtokenException(HTTPException):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    detail = "JWT token is incorrect"
-
-    def __init__(self):
-        super().__init__(status_code=self.status_code, detail=self.detail)
-
-
-class JWTtokenExpiredException(HTTPException):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    detail = "JWT token is expired"
-
-    def __init__(self):
-        super().__init__(status_code=self.status_code, detail=self.detail)
-
-
-class UserIsNotPresentException(HTTPException):
-    status_code = status.HTTP_401_UNAUTHORIZED
+class BookingException(HTTPException):
+    status_code = 500
     detail = ""
 
     def __init__(self):
         super().__init__(status_code=self.status_code, detail=self.detail)
 
 
-class UserIsAllredyRegistered(HTTPException):
+class UserUnauthorizedException(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "User is unauthorized"
+
+
+class IncorrectJWTtokenException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "JWT token is incorrect"
+
+
+class JWTtokenExpiredException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "JWT token is expired"
+
+
+class UserIsNotPresentException(BookingException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = ""
+
+
+class UserIsAllredyRegistered(BookingException):
     status_code = status.HTTP_401_UNAUTHORIZED
     detail = "User is allready registered"
 
-    def __init__(self):
-        super().__init__(status_code=self.status_code, detail=self.detail)
 
-
-class IncorrectEmailOrPasswordException(HTTPException):
+class IncorrectEmailOrPasswordException(BookingException):
     status_code = status.HTTP_401_UNAUTHORIZED
     detail = "Incorrect e-mail or password"
 
-    def __init__(self):
-        super().__init__(status_code=self.status_code, detail=self.detail)
 
-
-class NoFreeRoomsException(HTTPException):
+class NoFreeRoomsException(BookingException):
     status_code = status.HTTP_409_CONFLICT
     detail = "No free rooms"
 
-    def __init__(self):
-        super().__init__(status_code=self.status_code, detail=self.detail)
+
+class IncorrectDateValuesException(BookingException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "Incorrect date values: Departure date >= Arrival date"
+
+
+class BookingMoreThirtyDaysException(BookingException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "Arrival date - Departure date > 30 days"
